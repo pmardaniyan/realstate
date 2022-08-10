610be4e09b867b8e646b43da792d29fe2cdaf5e8
P_Mardaniyan <p.mardaniyan@gmail.com>
2022-08-10 09:52:07 +0430

    initial nextjs

---
+++ src/layout/Footer.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <div>
+            im footer website
+        </div>
+    )
+}
+
+export default Footer
---
+++ src/layout/Header.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <div>
+            im headers website
+        </div>
+    )
+}
+
+export default Header
